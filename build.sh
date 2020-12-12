e47f01ea536ceacac788dc097ad2460a854721f0
Abi Baxter <AbiABaxter@gmail.com>
2020-12-12 13:04:14 +0000

    Add shebang to build script

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
 error_flags="-Werror -Wall -Wno-unused-function"
 defines="-DINTERNAL_BUILD -DBUILD_LINUX -DUSE_OPENGL"
 modules="-DZEN2D -DZENTERMINAL"
 mkdir -p build
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo [ZenBuild] Generating Shaders
-pushd . > null
+pushd . &> /dev/null
 cd zenlib/zen2d/shaders
 python generate_opengl_shaders.py
-popd > null
+popd &> /dev/null
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo [ZenBuild] Building platform layer
 gcc $error_flags $modules $defines -g zenlib/zencore/zencore_linux.c -o build/out -lX11 -lGL -lGLU -lm -ludev -ldl
