524e23576f76aac279ee6414146dd75f9c6b7d63
Abi Baxter <AbiABaxter@gmail.com>
2021-02-27 15:38:26 +0000

    Remove all the boilerplate from build.bat

--- build.bat
+++ build.bat
@@ -5,12 +5,12 @@ set modules = "/DUSE_OPENGL /DZEN3D /DZEN2D"
 REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo [ZenBuild] Building platform layer
 pushd build
-cl -Zi %error_flags% /DINTERNAL_BUILD /DUSE_OPENGL /DZEN3D /DZEN2D ../zenlib/zencore/zencore_win32.c /link user32.lib gdi32.lib opengl32.lib winmm.lib 
+cl /nologo -Zi %error_flags% /DINTERNAL_BUILD /DUSE_OPENGL /DZEN3D /DZEN2D ../zenlib/zencore/zencore_win32.c /link user32.lib gdi32.lib opengl32.lib winmm.lib 
 popd
 REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo [ZenBuild] Building game layer
 pushd build
-cl -Zi %error_flags% /DINTERNAL_BUILD /DUSE_OPENGL /DZEN3D /DZEN2D ../sandbox.c /LD /link user32.lib gdi32.lib opengl32.lib winmm.lib 
+cl /nologo -Zi %error_flags% /DINTERNAL_BUILD /DUSE_OPENGL /DZEN3D /DZEN2D ../sandbox.c /LD /link user32.lib gdi32.lib opengl32.lib winmm.lib 
 
 popd
 REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
